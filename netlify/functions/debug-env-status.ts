4f01d58b17cf9713b3f9c4ec55e6d3600bc244f3
misinc <karim@misinc.net>
2026-05-22 13:38:01 -0600

    Add Netlify env diagnostics endpoint

---
+++ netlify/functions/debug-env-status.ts
@@ -0,0 +1,1 @@
+export { handler } from "../../src/backend/http/handlers/debug-env-status.js";
---
+++ src/backend/http/handlers/debug-env-status.ts
@@ -0,0 +1,30 @@
+import { getEnv } from "../../env.js";
+import { handlePreflight } from "../cors.js";
+import { json } from "../json.js";
+import { Handler } from "../types.js";
+
+export const handler: Handler = async (event) => {
+  const preflight = handlePreflight(event);
+  if (preflight) {
+    return preflight;
+  }
+
+  const env = getEnv();
+  return json(
+    200,
+    {
+      githubAppId: Boolean(env.githubAppId),
+      githubAppClientId: Boolean(env.githubAppClientId),
+      githubAppClientSecret: Boolean(env.githubAppClientSecret),
+      githubAppInstallationId: Boolean(env.githubAppInstallationId),
+      githubAppPrivateKey: Boolean(env.githubAppPrivateKey),
+      githubAppPrivateKeyLength: env.githubAppPrivateKey?.length ?? 0,
+      githubAccessToken: Boolean(env.githubAccessToken),
+      localMisRepoPath: Boolean(env.localMisRepoPath),
+      databaseUrl: Boolean(env.databaseUrl),
+      databaseUrlUnpooled: Boolean(env.databaseUrlUnpooled),
+      canonicalWebflowSiteId: env.canonicalWebflowSiteId
+    },
+    event
+  );
+};
